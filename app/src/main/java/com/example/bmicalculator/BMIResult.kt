dc4c08fc549c23dde8f3931dd5b0df06e5ced35e
rajasura03 <rrajasurya2003@gmail.com>
2023-08-30 10:06:07 +0530

    BMI-Calculator

---
+++ app/src/main/java/com/example/bmicalculator/BMIResult.kt
@@ -0,0 +1,110 @@
+package com.example.bmicalculator
+
+import android.animation.ArgbEvaluator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+class BMIResult : AppCompatActivity() {
+
+    private lateinit var tvBmi: TextView
+    private lateinit var tvBodyFat: TextView
+    private lateinit var tvBmiCategory: TextView
+    private lateinit var bntBack: Button
+
+    var bmi=0.0
+    var bmiCategory=""
+    var bodyFat=0.0
+
+    var age=1
+    var height=1.0
+    var weight=1.0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmiresult)
+
+        tvBmi=findViewById(R.id.tvBmi)
+        tvBodyFat=findViewById(R.id.tvBodyFat)
+        tvBmiCategory=findViewById(R.id.tvBmiCategory)
+
+        Toast.makeText(applicationContext,"Hiii, This is BMI Result", Toast.LENGTH_SHORT).show()
+        val bundle = intent.extras
+        if (bundle != null){
+            val ageTemp = bundle.getString("age")
+            val heightTemp = bundle.getString("height")
+            val weightTemp = bundle.getString("weight")
+            if(!(ageTemp.isNullOrBlank()))
+                age=ageTemp.toInt()
+            if(!(heightTemp.isNullOrBlank()))
+                height=heightTemp.toDouble()
+            if(!(weightTemp.isNullOrBlank()))
+                weight=weightTemp.toDouble()
+        }
+        bmi=(weight/ Math.pow((height / 100), 2.0))
+        bodyFat=(1.20*bmi)+(0.23*age)-5.4
+        if(age>=20)
+            bmiCategory=when(bmi){
+                in 0.0..16.0 -> "Severe Thinness"
+                in 16.0..18.0 -> "Moderate Thinness"
+                in 18.0..18.5 -> "Mild Thinness"
+                in 18.5..25.0 -> "Normal"
+                in 25.0..30.0 -> "Overweight"
+                in 30.0..35.0 -> "Obese Class I"
+                in 35.0..40.0 -> "Obese Class II"
+                else -> "Obese Class III"
+            }
+        else
+            bmiCategory=when(bodyFat){
+                in 0.0..5.0 -> "Underweight"
+                in 5.0..85.0 -> "Healthy weight"
+                in 85.0..95.0 -> "At risk of overweight"
+                else -> "Overweight"
+            }
+        tvBmi.text="%.2f".format(bmi)
+        tvBodyFat.text="%.2f".format(bodyFat)+"%"
+        tvBmiCategory.text=bmiCategory
+
+        if(bmi<18.6 && age>=20){
+            var color = ContextCompat.getColor(this, R.color.color_LowWeight) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else if(bmi<25.1 && age>=20){
+            var color = ContextCompat.getColor(this, R.color.color_Healthy) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else if(bmi<30.1 && age>=20) {
+            var color = ContextCompat.getColor(this, R.color.color_Fat) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else if(bmi>30.1 && age>=20) {
+            var color = ContextCompat.getColor(this, R.color.color_OverWeight) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+
+        else if(bodyFat<=5.0){
+            var color = ContextCompat.getColor(this, R.color.color_LowWeight) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else if(bodyFat<=85.0){
+            var color = ContextCompat.getColor(this, R.color.color_Healthy) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else if(bodyFat<=95.0){
+            var color = ContextCompat.getColor(this, R.color.color_Fat) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        else{
+            var color = ContextCompat.getColor(this, R.color.color_OverWeight) as Int
+            tvBmiCategory.setTextColor(color)
+        }
+        val bntBack=findViewById<Button>(R.id.bntBack)
+        bntBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etAge: EditText
+    private lateinit var etHeight: EditText
+    private lateinit var etWeight: EditText
+    private lateinit var rgGender: RadioGroup
+    private lateinit var radioButton: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etAge=findViewById(R.id.etAge)
+        etHeight=findViewById(R.id.etHeight)
+        etWeight=findViewById(R.id.etWeight)
+
+        rgGender=findViewById(R.id.rgGender)
+        rgGender.setOnCheckedChangeListener{ group, checkedId ->
+            radioButton=findViewById(checkedId)
+
+//            if(checkedId==R.id.rbMale){
+//                Toast.makeText(applicationContext,"Gender "+radioButton.text, Toast.LENGTH_SHORT).show()
+//            }
+//            else if(checkedId==R.id.rbFemale){
+//                Toast.makeText(applicationContext,"Gender "+radioButton.text, Toast.LENGTH_SHORT).show()
+//            }
+        }
+
+        val bntSubmit=findViewById<Button>(R.id.bntSubmit)
+        bntSubmit.setOnClickListener {
+
+//            Toast.makeText(applicationContext,"Button Clicked", Toast.LENGTH_SHORT).show()
+            var age=0
+            if(!(etAge.text.isNullOrBlank()))
+                age=(etAge.text).toString().toInt()
+            var height=0.0
+            if(!(etHeight.text.isNullOrBlank()))
+                height=(etHeight.text).toString().toDouble()
+            var weight=0.0
+            if(!(etWeight.text.isNullOrBlank()))
+                weight=(etWeight.text).toString().toDouble()
+
+            if(age>150 || age<1)
+                Toast.makeText(applicationContext,"Invalid Age "+etAge.text, Toast.LENGTH_SHORT).show()
+            else if(height>300.0 || height<1.0)
+                Toast.makeText(applicationContext,"Invalid Height "+etHeight.text, Toast.LENGTH_SHORT).show()
+            else if(weight>800.0 || weight<1.0)
+                Toast.makeText(applicationContext,"Invalid Weight "+etWeight.text, Toast.LENGTH_SHORT).show()
+            else {
+                val bundle = Bundle()
+                bundle.putString("age", (etAge.text).toString())
+                bundle.putString("height", (etHeight.text).toString())
+                bundle.putString("weight", (etWeight.text).toString())
+
+                val intent = Intent(this, BMIResult::class.java)
+                intent.putExtras(bundle)
+//                overridePendingTransition(R.anim.slide_to_top,R.anim.slide_from_bottom)
+                startActivity(intent)
+            }
+        }
     }
 }
